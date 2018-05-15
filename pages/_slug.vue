41c1eebac83795ffa4a95f7a07942204335cb612
Sean Brage <seanmbrage@me.com>
2018-05-15 09:00:55 -0600

    Add syntax highlighting to `<pre>` elements.
    Large code blocks wrapped in `<pre>` elements will now have highlight.js
    syntax highlighting applied to them, while independent `code` elements
    will retain the default Bulma styling.

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -2,14 +2,14 @@
   <section class="section">
     <div class="container">
       <div class="columns">
-        <div class="column is-8">
+        <div class="column is-10">
           <p class="subtitle is-6">
             <nuxt-link to="/">Back to All Posts</nuxt-link>
           </p>
 
           <h1 class="title is-2"> {{ post.fields.title }} </h1>
           <hr>
-          <div class="content" v-html="$md.render(post.fields.content)">
+          <div class="content" v-highlight v-html="$md.render(post.fields.content)">
           </div>
         </div>
       </div>
